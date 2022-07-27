0e6ae4e3d8081ffad97f8565507b9d11805ff55e
Zibo88 <vincenzo.tardino1988@gmail.com>
2022-07-27 14:43:53 +0200

    creata classe con attributi, create istanze che tramite __construct passano gli argomenti, assegnati gli attributi agli argomenti all'interno di __construct

--- index.php
+++ index.php
@@ -34,32 +34,25 @@ class Movie {
 
 }
 
-
-// definisco delle variabili utili nelle istanze
-$genreAction = 'Azione';
-$genreDramatic = 'Drammatico';
-$genreFantasy = 'Fantasy';
-$genreComedy = 'Commedia';
-
 // definisco le instanze
 $quartoPotere = new Movie('Quarto Potere', 'Drammatico', '1941');
-$quartoPotere->description = "La trama di Quarto potere (1941). Il magnate dell'informazione Charles Foster Kane pronuncia la parola 'Rosebud' immediatamente prima di morire, e un reporter va alla ricerca della soluzione dell'enigma nel suo passato...";
+
 var_dump($quartoPotere);
 
 $ilPadrino = new Movie('Il Padrino', 'Drammatico', '1972');
-$ilPadrino->description = "La trama di Il padrino (1972). Don Vito Corleone è il boss di una famiglia mafiosa di New York, che si è sempre limitata ad alcune attività tra cui il gioco d'azzardo. Quando il gangster sostenuto da un'altra famiglia, i Sollozzo, annuncia di voler intraprendere lo spaccio di droga su tutta New York, Don Vito si oppone e sarà l'inizio di una lunga serie di vendette...";
+
 var_dump($ilPadrino);
 
 $matrix = new Movie ('Matrix', 'Azione', '1999');
-$matrix->description = "Un hacker di nome Neo, grazie all'aiuto del misterioso Morpheus, scopre che quella per lui è la 'realtà' non è altro che un facciata, un mondo virtuale creato dal super computer Matrix per controllare gli esseri umani. Ma Morpheus è convinto che Neo sia il prescelto di cui parla un'antica profezia, e che sarà in grado di guidare la rivoluzione contro Matrix.";
+
 var_dump($matrix);
 
 $guardianiDellaGalassia = new Movie('Guardiani della Galassia', 'Fantascienza', '2014');
-$guardianiDellaGalassia->description = "La trama di Guardiani della Galassia (2014). Per sfuggire all’ostinato Ronan (Lee Pace), un essere malvagio la cui sfrenata ambizione minaccia l’intero universo, l'esploratore spaziale Peter Quill (Chris Pratt) è costretto ad una scomoda alleanza con quattro improbabili personaggi: Rocket, un procione mercenario dotato di una grande abilità nel maneggiare le armi e nell’ideare strategie di battaglia; Groot, un umanoide dalle sembianze di un albero con limitate capacità comunicative; la letale ed enigmatica Gamora (Zoe Saldana) e il vendicativo Drax il Distruttore (Dave Bautista). Peter farà di tutto per guidare questa squadra improvvisata in un’ultima disperata battaglia per salvare il destino della galassia.";
+
 var_dump($guardianiDellaGalassia);
 
 $fantozzi = new Movie('Fantozzi', 'Commedia', '1975');
-$fantozzi->description = "La trama di Fantozzi (1975). Le giornate del ragionier Ugo Fantozzi, più che un impiegato, uno zerbino umano: i colleghi lo sfruttano per farsi sbrigare il proprio lavoro, il collega ragionier Filini lo coinvolge in iniziative terrificanti, come partite di calcio tra scapoli ed ammogliati, gite aziendali e feste di fine anno. A casa neppure se la passa bene, il ragionier Fantozzi, con sua moglie e Mariangela, la loro terrificante bambina. Innamorato segretamente della collega, la vivace signorina Silvani, Fantozzi tenta sempre di invitarla a pranzo senza successo...";
+
 var_dump($fantozzi);
 
 ?>
